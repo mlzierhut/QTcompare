370662569c507dd8865b4e96d6965e4bfa25a616
Luke Fostvedt <lukefostvedt@gmail.com>
2015-08-01 15:37:28 -0700

    r file in progress to estimate different models

---
+++ Nonlinear Models.R
@@ -0,0 +1,37 @@
+###############################################################
+###############################################################
+#                                                             #
+#  VArious Nonlinear modeling approaches for QT prolongation  #
+#  When there is an observed Conc-RR effect                   #
+#  Code from Luke Fostvedt                                    #
+#                                                             #
+###############################################################
+###############################################################
+library(nlme) # for lme() function
+library(magrittr) # for the piping operator
+library(plyr)  # for data manipulation functions
+library(ggplot2) # for graphing, may not be used
+
+# model 1:
+# use a fixed correction for QTc without any adjustment for RR-conc relationship
+
+fixQTc <- 
+
+
+# model 1:
+# Simultaneously estimate a fixed correction for baseline and treatment
+# Treatment correction is for any concentration
+
+
+# model 1:
+# Simultaneously estimate a fixed correction for baseline and treatment
+# RR correction factor is a linear function of concentration
+  
+
+
+
+
+
+
+
+
